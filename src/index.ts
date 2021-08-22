91c98ea90d503f42e1f302515589b8e426f9364c
sp4cecadet <iventus2020@gmail.com>
2021-08-22 22:32:28 +0300

    API stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from "express";
-
 import express from "express";
 import mongoose from "mongoose";
 
-import { UserModel } from "./schemas/";
-import { UserController } from "./controllers/";
+import {
+  UserController,
+  DialogController,
+  MessageController,
+} from "./controllers/";
 
 const bodyParser = require("body-parser");
 
@@ -13,19 +14,40 @@ const PORT = 3000;
 const DBNAME = "chat";
 
 const User = new UserController();
+const Dialog = new DialogController();
+const Message = new MessageController();
 
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/" + DBNAME, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb://localhost:27017/" + DBNAME,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  () => {
+    console.log("[MONGODB]:", DBNAME, "AIR");
+  }
+);
 
 // User requests block
-app.get("/:id", User.index);
-app.post("/register", User.register);
-app.delete("/:id", User.delete);
+app.get("/user/:id", User.index);
+app.post("/user/register", User.register);
+app.delete("/user/:id", User.delete);
+// =====================
+
+// Dialog requests block
+app.get("/dialogs", Dialog.index);
+app.post("/dialog", Dialog.create);
+app.delete("/dialog/:id", Dialog.delete);
+// =====================
+
+// Message requests block
+app.get("/messages/:id", Message.index);
+app.post("/message", Message.create);
+app.delete("/message/:id", Message.delete);
 // =====================
 
 app.listen(PORT, () => {
